036ae38d463da72ac90aeb1232f175007343f56f
tjdekdms0215 <tjdekdms0608@gmail.com>
2026-07-11 18:57:57 +0900

    후기 작성 페이지 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -10,30 +10,19 @@ const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route
-                    path="/"
-                    element={<Home />}
-                />
+                <Route path="/" element={<Home />} />
 
-                <Route
-                    path="/kakao/auth-code"
-                    element={<AuthHandler />}
-                />
+                <Route path="/kakao/auth-code" element={<AuthHandler />} />
 
-                <Route
-                    path="/kakao/callback"
-                    element={<AuthHandler />}
-                />
+                <Route path="/kakao/callback" element={<AuthHandler />} />
 
-                <Route
-                    path="/guide"
-                    element={<Guide />}
-                />
+                <Route path="/guide" element={<Guide />} />
 
-                <Route
-                    path="/review/select"
-                    element={<ReviewSelect />}
-                />
+                <Route path="/review/select" element={<ReviewSelect />} />
+
+                <Route path="/review/write/:workspaceId" element={<ReviewWrite />} />
+
+                <Route path="/review/write/new" element={<ReviewWrite />} />
             </Routes>
         </BrowserRouter>
     );
